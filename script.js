a1cf836f915cc3a496628ac8b9f7162b692173e4
kpijenifer <jenifermech7@gmail.com>
2022-08-20 11:57:41 +0530

    add-file.

---
+++ script.js
@@ -0,0 +1,57 @@
+//math round
+console.log(Math.round(4.6));
+
+
+//math cell
+
+console.log(Math.ceil(4));
+
+//math floor
+
+console.log(Math.floor(4.4));
+
+
+//math trunc
+
+console.log(Math.trunc(4.6));
+
+//math sign
+console.log(Math.sign(-4.9));
+
+//math power
+console.log(Math.pow(5,3));
+
+
+//math sort
+
+console.log(Math.sqrt(64));
+
+//math absoulte
+
+console.log(Math.abs(-6.7));
+
+//math sign
+console.log(Math.sin(30)); 
+
+//math cos
+
+console.log(Math.cos(30));
+
+//math.max
+console.log(Math.max(10,20,30,40));
+//math min
+console.log(Math.min(10,20,30,40,50,60));
+
+//math random
+console.log(Math.random());
+
+//math log
+console.log(Math.log(15));
+
+//math.log2
+console.log(Math.log2(64));
+
+
+//math log10
+console.log(Math.log10(1000));
+
