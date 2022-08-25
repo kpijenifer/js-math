bbf4779e5ee59f071be7a29f6506905ccc457edc
kpijenifer <jenifermech7@gmail.com>
2022-08-25 16:26:37 +0530

    add-file.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //math round
-console.log(Math.round(4.6));
+       console.log(Math.round(4.6));
 
 
 //math cell
@@ -13,7 +13,7 @@ console.log(Math.floor(4.7));
 
 //math trunc
 
-console.log(Math.trunc(4.7));
+console.log(Math.trunc(4.77));
 
 //math sign
 console.log(Math.sign(4.9));
@@ -49,10 +49,10 @@ console.log(Math.random());
 console.log(Math.log(15));
 
 //math.log2
-console.log(Math.log2(32));
+console.log(Math.log2(32));	
 
 
-//math log10
+//math log10      
 console.log(Math.log10(1000));
 
 
@@ -80,8 +80,138 @@ let x=document.getElementById("caleb");
 	
 	
 	
-	let num = document.getElementsByName("animal").length;
-document.getElementById("demo").innerHTML = num;
+//	let num = document.getElementsByName("animal").length;
+//.getElementById("demo").innerHTML = num;
+
+//new date
+ let s=new Date();
+document.getElementById("jeni").innerHTML=s;
+console.log(s);
+
+//new date milliseconds
+
+let k=new Date(1000000000);
+
+document.getElementById("jeni").innerHTML=k;
+console.log(k);
+
+
+//new Date date string
+
+
+let v=new Date("oct,10,1970");
+//document.getElementById("jeni").innerHTML=v
+console.log(v);
+
+
+//new Date(year,month,day,hours,minutes,seconds,milliseconds)
+
+let g=new Date(2022,7,25,3,23,30,0);
+
+document.getElementById("jeni").innerHTML=g;
+console.log(g);
+
+//get full year
+
+let l=new Date();
+console.log(l.getFullYear());
+
+//get month
+let n=new Date();
+console.log(n.getMonth());
+
+
+//get date
+let m=new Date();
+console.log(m.setFullYear(2020));
+console.log(m);
+
+//gethours
+let y=new Date();
+console.log(y.getDate());
+
+//get hours
+
+let j=new Date();
+console.log(j.getHours());
+
+//get minutes
+
+let p=new Date();
+console.log(p.getMinutes());
+
+//get seconds
+
+let q=new Date();
+console.log(q.getSeconds());
+
+//get milliseconds
+
+let w=new Date();
+console.log(w.getMilliseconds());
+
+//get time
+let e=new Date();
+console.log(e.getTime());
+
+//get day
+let r=new Date();
+console.log(r.getDay());
+
+//get gate.now
+
+//let u=new Date();
+//console.log(u.get Date.now());
+
+//todate string
+
+let ads=new Date(1770,10,15);
+
+console.log(ads.toDateString());
+
+//toisostring
+
+let bds=new Date(2000,5,25);
+console.log(bds.toISOString());
+
+//tolocolestring
+
+let bgh=new Date();
+console.log(bgh.toLocaleDateString());
+
+//tolocaletimestring
+
+let akz=new Date();
+console.log(akz.toLocaleTimeString());
+
+//tolocalestring
+
+let kml=new Date();
+console.log(kml.toLocaleString());
+
+//to string
+let jkl=new Date();
+console.log(jkl.toString());
+
+//totimestring
+let f=new Date();
+console.log(akz.toTimeString());
+
+//toUTCstring
+
+let bnm=new Date();
+console.log(bnm.toUTCString());
+
+
+
+
+
+
+
+
+
+
+
 	
 
 
